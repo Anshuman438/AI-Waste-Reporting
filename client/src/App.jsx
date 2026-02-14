4b7aeff90b373004e23b263bf1fe1e9f55352390
Anshuman singh <anshumansingh5438@gmail.com>
2026-02-15 01:05:50 +0530

    login file name change term 3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import {
   useLocation,
 } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Login from "./pages/Login1";
 import ReportWaste from "./pages/ReportWaste";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AdminDashboard from "./pages/AdminDashboard";
